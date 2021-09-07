b8f7eb4dcb12e45f77e56192d2ba01eba746dcb7
Marian Gobea <gobeamariano@gmail.com>
2021-09-07 14:55:00 -0300

    ultima version con jupiters :)

--- juego_del_ahorcado.py
+++ juego_del_ahorcado.py
@@ -1,27 +1,28 @@
-import random
 import os
+import random
+
 
 def word():
-    with open("./archivos/data.txt", "r" , encoding="utf-8") as f:
-        words=[line for line in f] #List comprehensions
-    wordrandom=(random.choice(words)).upper()
+    with open("./archivos/data.txt", "r", encoding="utf-8") as f:
+        words = [line for line in f]  # List comprehensions
+    wordrandom = (random.choice(words)).upper()
     return wordrandom
 
 
 def lenword(word):
-    len_word=len(word)-1
-    presentation=["_ "]*len_word
+    len_word = len(word)-1
+    presentation = ["_ "]*len_word
     return presentation
 
 
-def changelarge(hiddenword,letter,index):
-    hiddenword[index]=letter
+def changelarge(hiddenword, letter, index):
+    hiddenword[index] = letter
     return hiddenword
 
 
 def imprimirpantalla(lifes):
     print("="*20+" Bienvenido al Hangman Game "+"="*20)
-    my_list_ASCII=['''
+    my_list_ASCII = ['''
                     +---+
                     |   |
                         |
@@ -71,48 +72,53 @@ def imprimirpantalla(lifes):
                    / \  |
                         |
                   ========= ''']
-    my_list_ASCII=my_list_ASCII[::-1]
+    my_list_ASCII = my_list_ASCII[::-1]
     print(my_list_ASCII[lifes])
 
 
 def play():
-    selectword=word()
-    large=lenword(selectword)
+    selectword = word()
+    large = lenword(selectword)
 
-    hits=0
-    objective=len(selectword)-1
-    lifes=6
-    usedletters=[]
+    hits = 0
+    objective = len(selectword)-1
+    lifes = 6
+    usedletters = []
 
     while lifes > 0 and hits < objective:
         imprimirpantalla(lifes)
         print("")
-        print("Adivine la siguiente palabra. La misma cuenta con "+str(len(selectword)-1)+" caracteres.")
+        print("Adivine la siguiente palabra. La misma cuenta con " +
+              str(len(selectword)-1)+" caracteres.")
         print(*large, sep=" ")
         print("")
-        letter=input("Ingrese una letra: ").upper()
+        letter = input("Ingrese una letra: ").upper()
         assert letter.isalpha(), "Debe introducir una letra. No numeros"
         while len(letter) != 1:
-            letter=input("Ingreso mas de un dígito. Ingrese solo un digito: ").upper()
+            letter = input(
+                "Ingreso mas de un dígito. Ingrese solo un digito: ").upper()
             assert letter.isalpha(), "Debe introducir una letra. No numeros"
         while letter in usedletters:
-            letter=input("Ingresó una letra repetida. Ingrese otra que no haya usado: ").upper()
+            letter = input(
+                "Ingresó una letra repetida. Ingrese otra que no haya usado: ").upper()
         usedletters.append(letter)
         print("Las letras usadas hasta aquí son: ")
         print(*usedletters, sep=" ")
-        hitsbefore=hits
-        for i in range(0,len(selectword)-1): 
+        hitsbefore = hits
+        for i in range(0, len(selectword)-1):
             if selectword[i] == letter:
                 hits += 1
-                print("Excelente. Ha acertado! Ha sumado 1 punto. Quedan "+str(objective-hits)+" letras por encontrar.")
-                large=changelarge(large,letter,i)
+                print("Excelente. Ha acertado! Ha sumado 1 punto. Quedan " +
+                      str(objective-hits)+" letras por encontrar.")
+                large = changelarge(large, letter, i)
 
         if hitsbefore == hits:
             lifes -= 1
-            print("No se encontró la letra elegida. Usted ha perdido una vida. Le quedan: "+str(lifes)+" vidas.")
-        
+            print(
+                "No se encontró la letra elegida. Usted ha perdido una vida. Le quedan: "+str(lifes)+" vidas.")
+
         input("Presiones Enter para continuar: ")
-        os.system("cls") 
+        os.system("cls")
 
     if hits == objective:
         imprimirpantalla(lifes)
@@ -120,7 +126,7 @@ def play():
     if lifes == 0:
         imprimirpantalla(lifes)
         print("Game Over. Vuelva a intentarlo mas tarde")
-        
+
 
 if __name__ == '__main__':
     play()
